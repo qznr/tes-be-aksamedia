00c345aea9ba394488700653327569f3f9916447
Moch. Gustav Ali Rafsandjani <mgustav.penting@gmail.com>
2025-01-03 08:17:06 +0700

    first commit

---
+++ database/migrations/2025_01_03_011421_create_employees_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Str;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('image')->nullable();
+            $table->string('name');
+            $table->string('phone');
+            $table->string('position');
+            $table->foreignUuid('division_id')->constrained('divisions')->onDelete('cascade');
+            $table->timestamps();
+        });
+
+        Schema::table('employees', function (Blueprint $table) {
+            $table->uuid('id')->default(Str::uuid())->change();
+        });
+    }
+
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('employees');
+    }
+};
